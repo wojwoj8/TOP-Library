6f072486377d3bede22da655de20dff3b988952c
wojwoj8 <wojwoj8@gmail.com>
2023-01-12 19:39:01 +0100

    Books data prints on page

--- script.js
+++ script.js
@@ -12,5 +12,17 @@ function addBookToLibrary(){
     //fds
 }
 const book1 = new Book('autor', 'ksiazka', "2137", 'True');
+const book2 = new Book('autodfsr', 'ksiazasdka', "21", 'False');
 myLibrary.push(book1);
-console.log(myLibrary);
+myLibrary.push(book2);
+//console.log(myLibrary);
+const tableBody = document.querySelector('tbody');
+
+for (let i = 0; i < myLibrary.length; i++){
+    tableBody.appendChild(document.createElement('tr')).innerHTML = 
+        `<td>${myLibrary[i].author}</td>
+         <td>${myLibrary[i].title}</td>
+         <td>${myLibrary[i].numberOfPages}</td>
+         <td>${myLibrary[i].read}</td>`;
+    console.log(myLibrary[i])
+}
