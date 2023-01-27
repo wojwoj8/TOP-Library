d14ac8d63c959af33f87fa8fe7ed30196b2b0393
wojwoj8 <wojwoj8@gmail.com>
2023-01-27 17:07:33 +0100

    have to make it again

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const myLibrary = [];
 const form = document.querySelector('.form');
 const tableBody = document.querySelector('tbody');
 const submit = document.querySelector('#sub');
+const inputs = document.querySelectorAll('input');
 
 function Book(author, title, numberOfPages, read) {
   this.author = author;
@@ -13,29 +14,50 @@ function Book(author, title, numberOfPages, read) {
 }
 
 function loop() {
-  tableBody.appendChild(document.createElement('tr')).innerHTML = `<td>${myLibrary[myLibrary.length - 1].author}</td>
+  const row = document.createElement('tr');
+  const butt = document.createElement('button');
+  butt.innerHTML = 'Remove';
+  butt.setAttribute('class', 'btn btn-secondary mx-auto', 'type', 'button');
+  butt.dataset.num = myLibrary.length - 1;
+  tableBody.appendChild(row).innerHTML = `<td>${myLibrary[myLibrary.length - 1].author}</td>
          <td>${myLibrary[myLibrary.length - 1].title}</td>
          <td>${myLibrary[myLibrary.length - 1].numberOfPages}</td>
          <td>${myLibrary[myLibrary.length - 1].read}</td>`;
-  console.log(myLibrary[myLibrary.length - 1]);
+  row.appendChild(butt);
+  document.querySelectorAll('[data-num]');
+  // console.log(myLibrary[myLibrary.length - 1]);
 }
 // submit.addEventListener('click', addBookToLibrary, false);
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+});
 function addBookToLibrary() {
+  // eslint-disable-next-line consistent-return
   submit.addEventListener('click', () => {
     console.log('test');
     const author = document.querySelector('input[name="author"]').value;
     const title = document.querySelector('input[name="title"]').value;
-    const number = document.querySelector('input[name="number"]').value;
+    let number = document.querySelector('input[name="number"]').value;
+    number = Number(number);
     const read = document.querySelector('#read').value;
+    console.log(typeof number);
+    if (author === '' || title === '' || typeof number !== 'number' || number <= 0) {
+      return false;
+    }
     console.log(author, title, number, read);
-    const book = Object.create(Book(author, title, number, read));
+    // to clear input field after creating element
+    inputs.forEach((inp) => {
+      // eslint-disable-next-line no-param-reassign
+      inp.value = '';
+    });
+    const book = new Book(author, title, number, read);
     console.log('stworzono obiekt');
 
     myLibrary.push(book);
     loop();
     // form.style.display = 'none';
     // event.preventDefault();
-    return false;
+    // return false;
   });
 }
 
@@ -50,9 +72,13 @@ if ((myLibrary.length) === 0) {
   loop();
 }
 
-// console.log(myLibrary.length);
-// const book2 = new Book('autodfsr', 'ksiazasdka', '21', 'False');
-// myLibrary.push(book2);
-// console.log(myLibrary);
-
-// loop();
+console.log(document.querySelectorAll('[data-num]'));
+document.querySelectorAll('[data-num]').forEach((elem) => elem.addEventListener('click', () => {
+  let tabNum = elem.dataset.num;
+  tabNum = Number(tabNum);
+  myLibrary.splice(tabNum, 1);
+  console.log(elem);
+  console.log(elem.parentElement);
+  elem.parentElement.remove();
+  console.log(myLibrary);
+}));
