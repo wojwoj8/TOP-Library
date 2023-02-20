f89fe78d99ebd129ddaa3e2dfc6d0e5f361683c7
wojwoj8 <wojwoj8@gmail.com>
2023-02-20 21:11:12 +0100

    read works

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ Book.prototype.info = function () {
   } else {
     readStatus = 'has been read';
   }
-  console.log(`${this.title} by ${this.author} ${this.numberOfPages} pages ${readStatus}`);
+  // console.log(`${this.title} by ${this.author} ${this.numberOfPages} pages ${readStatus}`);
 };
 
 function loop() {
@@ -38,6 +38,7 @@ function loop() {
 
     // console.log(myLibrary[i]);
   }
+  readButton();
   removeBook();
 }
 // submit.addEventListener('click', addBookToLibrary, false);
@@ -48,24 +49,24 @@ form.addEventListener('submit', (event) => {
 function addBookToLibrary() {
   // eslint-disable-next-line consistent-return
   submit.addEventListener('click', () => {
-    console.log('test');
+    // console.log('test');
     const author = document.querySelector('input[name="author"]').value;
     const title = document.querySelector('input[name="title"]').value;
     let number = document.querySelector('input[name="number"]').value;
     number = Number(number);
     const read = document.querySelector('#read').value;
-    console.log(typeof number);
+    // console.log(typeof number);
     if (author === '' || title === '' || typeof number !== 'number' || number <= 0) {
       return false;
     }
-    console.log(author, title, number, read);
+    // console.log(author, title, number, read);
     // to clear input field after creating element
     inputs.forEach((inp) => {
       // eslint-disable-next-line no-param-reassign
       inp.value = '';
     });
     const book = new Book(author, title, number, read);
-    console.log('stworzono obiekt');
+    // console.log('stworzono obiekt');
 
     myLibrary.push(book);
     loop();
@@ -78,14 +79,14 @@ function getRemButt() {
 function removeTable() {
   const r = getRemButt();
   r.forEach((e, index) => {
-    console.log(index);
+    // console.log(index);
     r[index].parentElement.parentElement.remove();
   });
 }
 function removeBook() {
   const r = getRemButt();
   r.forEach((e, index) => e.addEventListener('click', () => {
-    console.log(index);
+    // console.log(index);
     r[index].parentElement.parentElement.remove();
     myLibrary.unshift(myLibrary.splice(index, 1)[0]);
     myLibrary.shift();
@@ -97,6 +98,21 @@ function removeBook() {
     // removeBook();
   }));
 }
+
+function readButton() {
+  const button = document.querySelectorAll('#readButton');
+  button.forEach((e, index) => e.addEventListener('click', () => {
+    if (e.innerHTML === 'False') {
+      e.innerHTML = 'True';
+      myLibrary[index].read = 'True';
+    } else {
+      e.innerHTML = 'False';
+      myLibrary[index].read = 'False';
+    }
+    loop();
+  }));
+}
+
 document.querySelector('#add').addEventListener('click', addBookToLibrary());
 myLibrary.push(new Book('Tolkien', 'Hobbit', 342, 'False'));
 myLibrary.push(new Book('J.R.R. Tolkien', 'TLoTR', 1242, 'True'));
